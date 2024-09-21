17ef96ce85b3de6f9469effe3889e3d8506707c1
Austin Young <austineyoung2000@gmail.com>
2024-09-20 20:48:43 -0400

    akita: allow product copy files

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/akita/aam_compatibility_matrix.xml
 DEVICE_PATH := device/google/akita
 
